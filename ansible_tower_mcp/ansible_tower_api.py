eddfbc2381c302cbc90d61d733650914687d4398
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-10 02:03:24 -0600

    Bump version: 1.3.0 → 1.3.1

--- ansible_tower_mcp/ansible_tower_api.py
+++ ansible_tower_mcp/ansible_tower_api.py
@@ -8,7 +8,7 @@ from urllib.parse import urljoin
 import re
 import urllib3
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 
 class Api:
--- ansible_tower_mcp/ansible_tower_mcp.py
+++ ansible_tower_mcp/ansible_tower_mcp.py
@@ -34,7 +34,7 @@ from ansible_tower_mcp.middlewares import (
     JWTClaimsLoggingMiddleware,
 )
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
