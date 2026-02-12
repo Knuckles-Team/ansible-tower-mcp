0045bd0fd41c7206a5cd341b8c9b992105917f6e
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-11 21:40:23 -0600

    Bump version: 1.3.5 → 1.3.6

--- ansible_tower_mcp/ansible_tower_mcp.py
+++ ansible_tower_mcp/ansible_tower_mcp.py
@@ -34,7 +34,7 @@ from ansible_tower_mcp.middlewares import (
     JWTClaimsLoggingMiddleware,
 )
 
-__version__ = "1.3.5"
+__version__ = "1.3.6"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
