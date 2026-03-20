4444720388d3b9d7a98199dfb22d5403f628383d
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-19 23:47:11 -0500

    Bump version: 1.3.44 → 1.3.45

--- ansible_tower_mcp/__main__.py
+++ ansible_tower_mcp/__main__.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-# coding: utf-8
-from ansible_tower_mcp.mcp_server import ansible_tower_mcp
+from ansible_tower_mcp.agent_server import agent_server
 
 if __name__ == "__main__":
-    ansible_tower_mcp()
+    agent_server()
