5296916bc6bbbcfc6df0265a3be787fa54ea6992
knucklessg1 <knucklessg1@gmail.com>
2026-05-06 08:38:50 -0500

    Bump version: 1.7.0 → 1.8.0

--- ansible_tower_mcp/mcp_server.py
+++ ansible_tower_mcp/mcp_server.py
@@ -41,7 +41,7 @@ from starlette.responses import JSONResponse
 
 from ansible_tower_mcp.api_client import Api
 
-__version__ = "1.7.0"
+__version__ = "1.8.0"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
