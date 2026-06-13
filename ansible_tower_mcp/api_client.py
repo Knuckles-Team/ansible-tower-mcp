5e054421d6ba113ca446ffab4a707d5ccc9e53a8
knucklessg1 <knucklessg1@gmail.com>
2026-06-12 22:19:38 -0500

    Bump version: 1.32.0 → 1.33.0

--- ansible_tower_mcp/api_client.py
+++ ansible_tower_mcp/api_client.py
@@ -12,7 +12,7 @@ from ansible_tower_mcp.api.api_client_teams import Api as TeamsApi
 from ansible_tower_mcp.api.api_client_templates import Api as TemplatesApi
 from ansible_tower_mcp.api.api_client_users import Api as UsersApi
 
-__version__ = "1.32.0"
+__version__ = "1.33.0"
 
 
 class Api(
